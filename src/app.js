5f389a0fe99d80e3431d3c2218a867400f74e63a
mariaperrone <perronemar148@gmail.com>
2021-08-28 00:37:57 +0000

    Ejercicio terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+var final = [".com", ".es", ".uy", ".co", ".ar", ".net", ".us", ".io"];
+var text = "";
+
+for (let i = 0; i < pronoun.length; i++) {
+  var newpronoun = pronoun[i];
+  for (let j = 0; j < adj.length; j++) {
+    var newadj = adj[j];
+    for (let k = 0; k < noun.length; k++) {
+      var newnoun = noun[k];
+      for (let l = 0; l < final.length; l++) {
+        console.log(newpronoun + newadj + newnoun + final[l]);
+        text += "<h2>" + newpronoun + newadj + newnoun + final[l] + "</h2>";
+      }
+    }
+  }
+}
+console.log(text);
+document.getElementById("domain").innerHTML = text;
